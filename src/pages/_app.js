b8cb8aa6dc9f9052b104da69f6cb7b361a7a3d32
Steven-Yiran <60683228+Steven-Yiran@users.noreply.github.com>
2023-04-04 17:01:23 -0400

    added skeleton in _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,37 @@
-import "@/styles/globals.css";
+/* eslint-disable */
+import "../styles/globals.css";
+import Head from "next/head";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import styles from "../styles/MiddReddit.module.css";
+
+function MainApp({ Component, pageProps }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>MiddReddit</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div>
+          <h1>MiddReddit</h1>
+        </div>
+        <div>
+          <h2>Sidebar</h2>
+          <body>Display sub-reddit</body>
+        </div>
+        <div>
+          <h2>Post Display</h2>
+          <body>View posts in column</body>
+        </div>
+        {/* <Component /> */}
+      </main>
+      <main>
+        <h1></h1>
+      </main>
+
+      <footer>MiddReddit 2023</footer>
+    </div>
+  );
 }
+
+export default MainApp;
