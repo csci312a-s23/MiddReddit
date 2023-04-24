cad2159b2a244a9c8af5c3127da8bd9dddc278ae
Terry Luongo <trluongo@optimum.net>
2023-04-24 17:05:20 -0400

    made sidebar collapsible using material ui

---
+++ src/components/sidebar/categorySidebarComponents/SingleLevel.js
@@ -0,0 +1,15 @@
+//code adapted from https://stackoverflow.com/questions/63297109/nested-sidebar-menu-with-material-ui-and-reactjs
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+
+export default function SingleLevel({ item, goToCategory }) {
+  const handleClick = () => {
+    console.log(item);
+    goToCategory(item.id);
+  };
+  return (
+    <ListItem button onClick={handleClick}>
+      <ListItemText primary={item.name} />
+    </ListItem>
+  );
+}
