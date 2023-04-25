9630573ac3395fc6eac707d599c165b067d18796
Steven-Yiran <60683228+Steven-Yiran@users.noreply.github.com>
2023-04-25 19:56:44 -0400

    edited code style in post creator

--- src/pages/posts/create.js
+++ src/pages/posts/create.js
@@ -1,4 +1,5 @@
 import Editor from "../../components/Editor";
+import PropTypes from "prop-types";
 import { useRouter } from "next/router";
 
 export default function PostCreator({
@@ -19,7 +20,6 @@ export default function PostCreator({
       };
       const response = await fetch("/api/posts", params);
       if (response.ok) {
-        //console.log(response.json());
         const newPost = await response.json();
         goToPost(newPost);
       }
@@ -28,35 +28,19 @@ export default function PostCreator({
     }
   };
 
-  //   const handleComplete = (newPost) => {
-  //     if (newPost) {
-  //       fetch("/api/articles", {
-  //         method: "POST",
-  //         body: JSON.stringify(newArticle),
-  //         headers: new Headers({
-  //           Accept: "application/json",
-  //           "Content-Type": "application/json",
-  //         }),
-  //       })
-  //         .then((resp) => resp.json())
-  //         .then((data) => setCurrentArticle(data))
-  //         .catch((err) => console.log(err));
-  //     } else {
-  //       router.back();
-  //     }
-  //   };
-
   return (
-    <>
+    <main>
       <Editor
         complete={complete}
         setCreatePost={setCreatePost}
         setOpenRightSideBar={setOpenRightSideBar}
       />
-    </>
+    </main>
   );
 }
-/*
+
 PostCreator.propTypes = {
-  setCurrentArticle: PropTypes.func.isRequired,
-}; */
+  goToPost: PropTypes.func.isRequired,
+  setOpenRightSideBar: PropTypes.func.isRequired,
+  setCreatePost: PropTypes.func.isRequired,
+};
