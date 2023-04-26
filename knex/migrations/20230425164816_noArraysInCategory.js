b36fd9a95b320adc654e5d62429ac0bdf7d3d438
Terry Luongo <trluongo@optimum.net>
2023-04-26 14:24:21 -0400

    fixed post-category relation, was a require loop

---
+++ knex/migrations/20230425164816_noArraysInCategory.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.table("Category", (table) => {
+    table.dropColumn("relatedPosts");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.table("Category", (table) => {
+    table.specificType("relatedPosts", "INT[]");
+  });
+};
