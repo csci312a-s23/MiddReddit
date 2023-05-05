7b2274b60a6ce2c61e6fc1f20fdbb3f5f2711745
Terry Luongo <trluongo@optimum.net>
2023-05-05 19:47:39 -0400

    comments now work, simple html layout

--- src/pages/api/posts/[id]/index.js
+++ src/pages/api/posts/[id]/index.js
@@ -8,14 +8,21 @@ import { onError } from "../../../../lib/middleware";
 import Post from "../../../../../models/Post";
 // Notice the `onError` middleware for aspect-oriented error handler. That middleware
 // will be invoked if the handler code throws an exception.
+
+function r(expr, depth) {
+  return expr.replace("?", depth > 0 ? `.${  r(expr, depth - 1)}` : "");
+}
 const handler = nc({ onError })
   .get(async (req, res) => {
     try {
       const post = await Post.query()
         .findById(req.query.id)
         .withGraphJoined(
-          "[category, comments.[parent,children.^3, author,post]]"
+          `[category, comments.${r("[children?, author, post,parent]", 4)}]`
         )
+        //`[category, comments.${r('[children?, author]', 0)}, author,post]]`//          "[category, comments.[parent,children.^3, author,post]]"
+        //`[category, comments.${r('[children?, author]', 0)}, author,post]]`//
+
         .modifyGraph("comments", (builder) => {
           builder.where("parentId", null);
         })
@@ -24,34 +31,34 @@ const handler = nc({ onError })
     } catch (error) {
       console.log(error);
     }
-    /* 
+  })
+  /*
     ...
   .query()
-  .eager(`[children.${r('[children?, pets]', 3)}, pets]`)
+  .eager(`[children.${r('[children?, author,post,parent]', 3)}, pets]`)
 ...
+*/
 
-function r(expr, depth) {
-  return expr.replace('?', depth > 0 ? '.' + r(expr, depth - 1) : '');
-  */ //implenet this, so I can have nested comments have names also
+  //implement this, so I can have nested comments have names also
 
-    // let query = Post.query();
-    // if (req.query.section) {
-    //   query = query.whereRaw("UPPER(SUBSTRING(title, 1, 1)) = req.query.section", [
-    //     req.query.section,
-    //   ]);
-    // }
-    // const posts = await query;
-    // res.status(200).json(posts);
+  // let query = Post.query();
+  // if (req.query.section) {
+  //   query = query.whereRaw("UPPER(SUBSTRING(title, 1, 1)) = req.query.section", [
+  //     req.query.section,
+  //   ]);
+  // }
+  // const posts = await query;
+  // res.status(200).json(posts);
 
-    // const { id, ...updatedPost } = req.body;
-    // // req.query.id is a string, and so needs to be converted to an integer before comparison
-    // if (id !== parseInt(updatedPost.id, 10)) {
-    //   // Verify id in the url, e.g, /api/posts/10, matches the id the request body
-    //   res.status(400).end(`URL and object does not match`);
-    //   return;
-    // }
-    // // Update the database ...
-  })
+  // const { id, ...updatedPost } = req.body;
+  // // req.query.id is a string, and so needs to be converted to an integer before comparison
+  // if (id !== parseInt(updatedPost.id, 10)) {
+  //   // Verify id in the url, e.g, /api/posts/10, matches the id the request body
+  //   res.status(400).end(`URL and object does not match`);
+  //   return;
+  // }
+  // // Update the database ...
+
   .put(async (req, res) => {
     // endpoint to update a new post
     const { id, ...updatePost } = req.body;
