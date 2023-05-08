5f57e0bae2cde79b97715b630ff41c38ed3539ca
Steven-Yiran <60683228+Steven-Yiran@users.noreply.github.com>
2023-05-08 00:22:08 -0400

    full component demo done

--- src/components/TopPosts.js
+++ src/components/TopPosts.js
@@ -3,7 +3,35 @@
 
     This define a component to display the trending posts
 */
+import { Card, CardContent } from "@mui/material";
+import { useEffect, useState } from "react";
 
 export default function TopPosts({}) {
-  return <div>TopPosts! Will Display Top 5 posts sorted by upvote count</div>;
+  const [topPosts, setTopPosts] = useState();
+
+  /* onload fetch trending posts */
+  useEffect(() => {
+    fetch("/api/posts?top=10")
+      .then((res) => res.json())
+      .then((data) => {
+        setTopPosts(data);
+      });
+  });
+
+  let postDisp;
+  if (topPosts) {
+    postDisp = topPosts.map((post) => (
+      <Card key={post.id} variant="outlined">
+        <CardContent>{post.title}</CardContent>
+      </Card>
+    ));
+  }
+  /* map trending posts to MUI cards */
+
+  return (
+    <div>
+      <h1>Trending Posts</h1>
+      {postDisp}
+    </div>
+  );
 }
